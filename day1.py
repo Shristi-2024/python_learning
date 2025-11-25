08c59d914296bf3044472f9d36af9c80dbdf310a
Shristi-2024 <shristi.kumari@cognizant.com>
2025-11-25 15:16:11 +0530

    Update 1

--- day1.py
+++ day1.py
@@ -1,1 +1,8 @@
-print("hello world")
+# Reverse a String without Slicing
+def reverse_string(s):
+    reversed_str = ""
+    for char in s:
+        reversed_str = char + reversed_str
+    return reversed_str
+
+print(reverse_string("My name is Shristi Kumari"))
