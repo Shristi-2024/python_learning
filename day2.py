7b9614983ea966c06539faea7b558ca2747f3b00
Shristi-2024 <shristi.kumari@cognizant.com>
2025-11-26 13:16:11 +0530

    add day2.py to project folder

---
+++ day2.py
@@ -0,0 +1,12 @@
+def is_anagram(str1, str2):
+    str1 = str1.lower()
+    str2 = str2.lower()
+    return sorted(str1) == sorted(str2)
+
+s1 = "Listen"
+s2 = "Silent"
+
+if is_anagram(s1, s2):
+    print(f'"{s1}" and "{s2}" are anagrams.')
+else:
+    print(f'"{s1}" and "{s2}" are not anagrams.')
---
+++ day3.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+language = input("Enter a programming language you are learning: ")
+
+print(f"Hello, my name is {name} and I am learning {language}.")
