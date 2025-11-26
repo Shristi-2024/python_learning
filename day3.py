1dfb2cbbc340c1669fa97c0b35692154e0b4c3f4
Shristi-2024 <shristi.kumari@cognizant.com>
2025-11-26 13:24:09 +0530

    update 1

--- day3.py
+++ day3.py
@@ -1,4 +1,6 @@
+# Format user input into a sentence
 name = input("Enter your name: ")
 language = input("Enter a programming language you are learning: ")
 
+# using format string
 print(f"Hello, my name is {name} and I am learning {language}.")
