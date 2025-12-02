fdfc83617bff95e40201b46d67b1c5b0f72a41f3
Shristi-2024 <shristi.kumari@cognizant.com>
2025-12-02 10:06:52 +0530

    update 1

---
+++ Week 2/day6.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    else:
+        if n == 0 or n == 1:   # base case
+            return 1
+        else:                  # recursive case
+            return n * factorial(n - 1)
+
+num = int(input("Enter a number: "))
+
+result = factorial(num)
+print(f"Factorial of {num} is: {result}")
